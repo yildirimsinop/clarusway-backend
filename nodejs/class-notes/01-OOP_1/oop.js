a71f2c2f6ab00f7b2c4ea238f9edce2e26c1e7f8
Umit Yildirim <umity@uia.no>
2023-09-13 21:00:32 +0200

    destra

--- nodejs/class-notes/01-OOP_1/oop.js
+++ nodejs/class-notes/01-OOP_1/oop.js
@@ -1,14 +1,51 @@
-// "use strict";
+"use strict";
+
+/* -------------------------------------------------------
+    OBJECTS
+------------------------------------------------------- */
 
 // const exampleObject = {
-//   propertyName: "value", // field, attribute
-//   methodName: function () {
-//     return "This is Method";
+
+//     propertyName: 'value', // field, attribute
+//     methodName: function () {
+//         return 'This is Method'
+//     }
+// }
+
+// console.log( exampleObject.propertyName )
+// console.log( exampleObject.methodName() )
+
+/* ------------------------------------------------------- */
+
+// const Car = {
+//   brand: "Ford",
+//   model: "Mustang",
+//   year: 1967,
+//   isAutoGear: true,
+//   colors: ["white", "red"],
+//   details: {
+//     color1: "red",
+//     color2: "white",
+//     engineSize: 4900,
+//   },
+//   startEngine: function () {
+//     return "Motor çalıştı";
 //   },
 // };
 
-// console.log(exampleObject.propertyName);
-// console.log(exampleObject.methodName);
+// console.log( Car.brand )
+// console.log( Car.colors )
+// console.log( Car.colors[0] )
+// console.log( Car.details )
+// console.log( Car.details.engineSize )
+// console.log( Car.startEngine() )
+
+// console.log(Car["brand"]);
+// console.log(Car.details["engineSize"]);
+// console.log(Car["details"]["engineSize"]);
+// console.log(Car["startEngine"]());
+
+/* ------------------------------------------------------- */
 
 const Car = {
   brand: "Ford",
@@ -22,16 +59,12 @@ const Car = {
     engineSize: 4900,
   },
   startEngine: function () {
-    return "Motor çalisti";
+    return "Motor çalıştı";
+  },
+
+  getDetails: function () {
+    //return this
+    return this.brand + " " + this.model + " " + this.year;
+    return this.startEngine();
   },
 };
-
-// console.log(Car.brand);
-// console.log(Car.colors);
-// console.log(Car.colors[0]);
-// console.log(Car.details);
-// console.log(Car.details.engineSize);
-// console.log(Car.startEngine());
-
-console.log(Car.details["engineSize"]);
-console.log(Car["details"]["en"]);
