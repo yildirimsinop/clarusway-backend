341bb626941f94a9ff96b31a5ebbf88f5573e90b
Umit Yildirim <umity@uia.no>
2023-09-14 22:12:13 +0200

    index

--- nodejs/class-notes/02-OOP_2/module.js
+++ nodejs/class-notes/02-OOP_2/module.js
@@ -1,7 +1,38 @@
 "use strict";
 
-//? Baska bir JS dosyasini icer aktarma
-// require("./modules/index.js");
-// require("./modules/index");
-const test = require("./module");
-test();
+/* -------------------------------------------------------
+    MODULES
+------------------------------------------------------- *
+//? Başka bir JS dosyasını içe aktarma:
+
+require('./module/index.js')
+require('./module/index')
+require('./module/')
+
+/* ------------------------------------------------------- *
+
+// Import:
+const test = require('./module/')
+test()
+
+/* ------------------------------------------------------- */
+
+// Shorthand for singleFunction:
+// require('./module/')()
+
+/* ------------------------------------------------------- */
+
+// const [ test1, test2, test3 ] = require('./module/')
+// test1(), test2(), test3()
+
+// const { test1, test2, test3 } = require('./module/')
+// test1(), test2(), test3()
+
+const { test1: newFunc, test2, test3, variable } = require("./module/");
+newFunc(), test2(), test3(), console.log(variable);
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
--- nodejs/class-notes/02-OOP_2/module/index.js
+++ nodejs/class-notes/02-OOP_2/module/index.js
@@ -2,14 +2,85 @@
 
 /* -------------------------------------------------------
     MODULES
-------------------------------------------------------- */
+------------------------------------------------------- *
 
-console.log("index.js çalıştı.");
+// console.log('index.js çalıştı.')
+
+const test = function() {
+    console.log('index.js çalıştı.')
+}
+
+// EXPORT:
+module.exports = test
+
+/* ------------------------------------------------------- *
+
+// EXPORT:
+module.exports = function () {
+    console.log('index.js çalıştı.')
+}
+
+/* ------------------------------------------------------- *
+
+const test1 = function() {
+    console.log('test1 çalıştı.')
+}
+
+const test2 = function() {
+    console.log('test2 çalıştı.')
+}
+
+const test3 = function() {
+    console.log('test3 çalıştı.')
+}
+
+// module.exports = [
+//     test1,
+//     test2,
+//     test3
+// ]
+
+// module.exports = {
+//     test1: test1,
+//     test2: test2,
+//     test3: test3,
+// }
+
+module.exports = {
+    test1,
+    test2,
+    test3,
+}
+
+/* ------------------------------------------------------- *
+
+module.exports.test1 = function() {
+    console.log('test1 çalıştı.')
+}
+
+module.exports.test2 = function() {
+    console.log('test2 çalıştı.')
+}
+
+module.exports.test3 = function() {
+    console.log('test3 çalıştı.')
+}
+
+module.exports.variable = 'new-value'
 
 /* ------------------------------------------------------- */
-const test = function () {
-  console.log("index.js calistiiii.");
+
+module.exports = {
+  test1: function () {
+    console.log("test1 çalıştı.");
+  },
+  test2: function () {
+    console.log("test2 çalıştı.");
+  },
+  test3: function () {
+    console.log("test3 çalıştı.");
+  },
+  variable: "new-value",
 };
 
-//?EXPORT
-module.exports = test;
+/* ------------------------------------------------------- */
