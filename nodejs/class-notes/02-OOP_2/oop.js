0c8edb9a1752dadc3c9b81b2f217f77b3d39dbee
Umit Yildirim <umity@uia.no>
2023-09-14 19:18:56 +0200

    oop2

---
+++ nodejs/class-notes/02-OOP_2/oop.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*-----------------------------------------------------------------------------
+
+OOP & classes
+-------------------------------------------------------------------------------*/
+//? OOP: Object Oriented Programming
+//? DRY: Don't Repeat Yourself
+//? BLUEPRINT: Taslak (Mimarların kullandığı mavi şablon kağıdı)
+//? CLASS: Obje türetmek için kullanılacak şablon.
