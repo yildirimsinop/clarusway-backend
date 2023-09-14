23558abea931a3c0829e6b885f99a3449eefcaf8
Umit Yildirim <umity@uia.no>
2023-09-14 21:39:07 +0200

    abtrak

--- nodejs/class-notes/02-OOP_2/oop.js
+++ nodejs/class-notes/02-OOP_2/oop.js
@@ -120,90 +120,131 @@ console.log (fordCliamate)
 //? Overload: Üst metodla aynı isimde ama farklı yapıda (parametre adet/tip) yeni method oluşturma. (aynı anda ikiside aktif) (JS desteklemez)
 
 class Vehicle {
-  vehicleIsActive = false;
 
-  constructor(vehicleType) {
-    this.vehicleType = vehicleType;
-  }
+    vehicleIsActive = false
 
-  getDetails() {
-    console.log("Vehicle getDetails çalıştı");
-    return this.vehicleType;
-  }
+    constructor(vehicleType) {
+        this.vehicleType = vehicleType
+    }
+
+    getDetails() {
+        console.log('Vehicle getDetails çalıştı')
+        return this.vehicleType
+    }
 }
 
 class Car extends Vehicle {
-  isRunning = false;
 
-  constructor(brand, model, year, vehicleType = "Car") {
-    //? super() parametresi en tepede olmalı (Önce parent constructor çalıştırılmalı)
-    super(vehicleType); // run constructor of ParentClass
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-  }
+    isRunning = false
 
-  runEngine() {
-    this.isRunning = true;
-    console.log("Motor Çalıştı");
-    return this.isRunning;
-  }
+    constructor(brand, model, year, vehicleType = 'Car') {
+        //? super() parametresi en tepede olmalı (Önce parent constructor çalıştırılmalı)
+        super(vehicleType) // run constructor of ParentClass
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
 
-  //? Override: Üstteki method ile aynı isimde. Artık bu geçerli.
-  getDetails() {
-    console.log("Car getDetails çalıştı");
-    // return this
-    return {
-      brand: this.brand,
-      model: this.model,
-      year: this.year,
-      vehicleType: super.getDetails(), // Parent class metodları super ile çalıştırabilir.
-      // vehicleIsActive: super.vehicleIsActive // super constructor bu veriyi this'e aktardı.
-      vehicleIsActive: this.vehicleIsActive, // super constructor bu veriyi this'e aktardı.
-    };
-  }
+    runEngine() {
+        this.isRunning = true
+        console.log('Motor Çalıştı')
+        return this.isRunning
+    }
+
+    //? Override: Üstteki method ile aynı isimde. Artık bu geçerli.
+    getDetails() {
+        console.log('Car getDetails çalıştı')
+        // return this
+        return {
+            brand: this.brand,
+            model: this.model,
+            year: this.year,
+            vehicleType: super.getDetails(), // Parent class metodları super ile çalıştırabilir.
+            // vehicleIsActive: super.vehicleIsActive // super-constructor bu veriyi this'e aktardı.
+            vehicleIsActive: this.vehicleIsActive // super constructor bu veriyi this'e aktardı.
+        }
+    }
 }
 
-const ford = new Car("Ford", "Mustang", 1967, "SUV");
-console.log(ford);
-console.log(ford.getDetails());
+const ford = new Car('Ford', 'Mustang', 1967, 'SUV')
+console.log ( ford )
+console.log ( ford.getDetails() )
+
+/* ------------------------------------------------------- *
+//? JS PUBLIC: Genel erişime açık.
+//? JS PROTECTED: Sadece Tanımlı olduğu class ve Inherit edilen child-class erişebilir.
+//? -* JS/ES12 öncesi desteklemiyor: Genel erişime açık ama lütfen dokunmayın :) 
+//? JS PRIVATE: Sadece tanımlı olduğu class içinde erişim var.
 
 
-/* ------------------------------------------------------- */
 class Vehicle {
-  vehicleIsActive = false; //? Public
-  _protectedProp = true; //? Protected property
-  #privateProp = true; //? Private Property
 
-  constructor(vehicleType) {
-    this.vehicleType = vehicleType;
-  }
-}
+    vehicleIsActive = false // PUBLIC PROPERTY
+    _protectedProp = true // PROTECTED PROPERTY
+    #privateProp = true // PRIVATE PROPERTY
 
-// Override yapma lütfen:
+    constructor(vehicleType) {
+        this.vehicleType = vehicleType
+    }
+
+    // Override yapma lütfen:
     _protectedMethod() {
-        console.log('Vehicle.protectedMethod()')
+        console.log('Vehicle protectedMethod çalıştı')
         return true
     }
 
     #privateMethod() {
-        console.log('Vehicle.privateMethod()')
+        console.log('Vehicle privateMethod çalıştı')
         return true
     }
 
     getDetails() {
-        console.log('Vehicle.getDetails()')
-        this.#privateMethod()
-        return this.vehicleType + ' is ' + this.privateProp
-        return this.vehicleType + ' is ' + this.#privateProp // Undefined.
+        console.log('Vehicle getDetails çalıştı')
+        console.log( 'privateProp', this.#privateProp )
+        console.log( this.#privateMethod() )
     }
+}
 
 class Car extends Vehicle {
-  isRunning = false;
 
-  constructor(brand, model, year, vehicleType = "Car") {
-    //? super() parametresi en tepede olmalı (Önce parent constructor çalıştırılmalı)
-    super(vehicleType); // run constructor of ParentClass
+    isRunning = false
+
+    constructor(brand, model, year, vehicleType = 'Car') {
+        super(vehicleType)
+        this.brand = brand
+        this.model = model
+        this.year = year
+    }
+
+    runEngine() {
+        this.isRunning = true
+        console.log('Motor Çalıştı')
+        return this.isRunning
+    }
+
+    getDetails() {
+        console.log('Car getDetails çalıştı')
+        // console.log( 'privateProp', this.#privateProp ) // NO ACCESS
+        // console.log( this.#privateMethod() ) // NO ACCESS
+        // console.log( 'privateProp', super.#privateProp ) // NO ACCESS
+        // console.log( super.#privateMethod() ) // NO ACCESS
+    }
+}
+
+const ford = new Car('Ford', 'Mustang', 1967, 'SUV')
+console.log ( ford )
+console.log ( ford.getDetails() )
+// console.log ( ford.#privateProp ) // NO ACCESS
+
+/* ------------------------------------------------------- */
+//? GETTER & SETTER METHODS: Görevi veri getirme (getter) ve veri güncelleme (setter) olan metodlardır.
+//? "STATIC" KEYWORD: Class'dan direkt erişim. (Instance erişemez.)
+
+class Car {
+  isRunning = false;
+  #price;
+
+  constructor(brand, model, year) {
     this.brand = brand;
     this.model = model;
     this.year = year;
@@ -211,8 +252,55 @@ class Car extends Vehicle {
 
   runEngine() {
     this.isRunning = true;
-    console.log("Motor Çalıştı");
+    console.log("Motor Çalıştırıldı");
     return this.isRunning;
+  }
+
+  set setPrice(newPrice) {
+    // return this.#price = newPrice
+    this.#price = newPrice;
+    console.log("Fiyat Alındı.");
+    return true;
+  }
+
+  get getPrice() {
+    // return this.#price
+    return "Fiyat: " + (this.#price ?? "Henüz Belirlenmedi.");
+  }
+
+  //? Direkt class ile erişmek istediklerimizi static ile işaretleriz.
+  //? Statik property veya methodlara intance ile erişilmez.
+  static staticProp = "static value";
+
+  //? Static methodlarda this ifadesi sadece statikleri çağırır.
+  static staticMethod() {
+    console.log("Static Method çalıştı");
+    return this; //? Static method içindeki this: sadece statik verileri verir.
   }
 }
 
+const ford = new Car("Ford", "Mustang", 1967);
+console.log(ford);
+
+console.log(ford.getPrice);
+// ford.price(5000) // CLassic method
+ford.setPrice = 5000; // Setter method
+console.log(ford.getPrice);
+
+//? STATIC METHOD EXAMPLE
+// Car.runEngine() // NO ACCESS
+const abc = Math.round(1.56);
+console.log(abc);
+
+//? Statikler instance'a aktarılmaz.
+// console.log(ford.staticMethod())
+console.log(Car.staticProp);
+console.log(ford);
+console.log(Car.staticMethod());
+
+/* ------------------------------------------------------- */
+//? ABSTRACTION: Soyutlama/Modelleme (Aynı amaç için kullanılan değişken ve methodları bir class içinde yazıyor olması)
+//? ENCAPCULLATION: Kapsülleme/Ayrıştırma (Kodların gizliliği, private attre erişilemiyor olması ve birbirinden bağımsız çalışmaları.)
+/* ------------------------------------------------------- */
+
+//! HAPPY CODDING :)
