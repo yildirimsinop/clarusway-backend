13d35e33c1b0dc357aa17b189e2688b13628eeef
Umit Yildirim <umity@uia.no>
2023-09-16 13:07:06 +0200

    env

--- nodejs/class-notes/03-NodeJS/index.js
+++ nodejs/class-notes/03-NodeJS/index.js
@@ -1,55 +1,35 @@
-"use strict";
-
-/* --------------------------------------------
-
-        NODEJS
-
--------------------------------------------- */
-const http = require("node:http"); // builtin: https://nodejs.org/api/http.html
-/* -------------------------------------------- *
-
-// http.createServer((req, res) => { ... }
-const app = http.createServer((request, response) => {
-
-    response.end('<h1> Welcome to NodeJS Server </h1>')
-
-})
-
-// http://127.0.0.1:8000 
-// http://localhost:8000 
-app.listen(8000, () => console.log('Server Runned: http://127.0.0.1:8000'))
-
-/* -------------------------------------------- */
-
-// const app = http
-//   .createServer((req, res) => {
-//     // console.log( req )
-//     // console.log( res )
-//     if (req.url == "/") {
-//       res.end("<h1>Main Page");
-//     } else if (req.url == "/second") {
-//       res.end("<h1>Second Page");
-//     } else {
-//       res.end("<h1>Server is running");
-//     }
-//   })
-//   .listen(8000, () => console.log("Server Runned: http://127.0.0.1:8000"));
-
-// /* -------------------------------------------- */
-
 http
   .createServer((req, res) => {
     if (req.url == "/") {
-      res.statusCode = 404;
-      res.statusMessage = "Not Found";
-      res.write("<p>Satir1</p>");
-      res.write("<p>Satir2</p>");
-      res.write("<p>Satir3</p>");
-      res.write("<p>Satir4</p>");
+      res.statusCode = 404; // Default: 200
+      res.statusMessage = "Not Found"; // Default: OK
 
+      res.setHeader("Content-Type", "text/html");
+      res.setHeader("another-header", "another-value");
+
+      res.write("* Satır1");
+      res.write("* Satır2");
+      res.write("* Satır3");
       res.end();
+    } else if (req.url == "/api") {
+      if (req.method == "GET") {
+        // res.writeHead(200, {
+        //     'Content-Type': 'application/json',
+        //     'another-header': 'another-value',
+        // })
+
+        // OVERLOAD GOOD EXAMPLE:
+        res.writeHead(200, "Status Message", {
+          "Content-Type": "application/json",
+          "another-header": "another-value",
+        });
+
+        res.end("OK");
+      } else {
+        res.end("Not supporting different method.");
+      }
     } else {
-      res.end("Server is runnning");
+      res.end("Server is running");
     }
   })
   .listen(8000, () => console.log("http://127.0.0.1:8000"));
