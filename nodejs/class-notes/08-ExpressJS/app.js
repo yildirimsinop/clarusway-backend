b4f01bc38bdff3eb0ef71148985ca94ea0889982
Umit Yildirim <umity@uia.no>
2023-09-25 19:58:20 +0200

    use

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -10,3 +10,10 @@
 /* ExpressJS Start */
 const express = require("express"); // Assing expressFramework to express variable.
 const app = express(); // run application on express.
+
+/* ENV */
+require("dotenv").config();
+const HOST = process.env.HOST || "http://localhost";
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Running on ${HOST} : ${PORT}`));
