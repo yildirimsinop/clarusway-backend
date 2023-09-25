06435960e2714af15bf3f0e8b165511c023d15a4
Umit Yildirim <umity@uia.no>
2023-09-25 19:35:37 +0200

    app.js

---
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -0,0 +1,12 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS
+------------------------------------------------------- */
+/*
+ * npm init -y
+ * npm i express dotenv
+ */
+
+/* ExpressJS Start */
+const express = require("express"); // Assing expressFramework to express variable.
+const app = express(); // run application on express.
