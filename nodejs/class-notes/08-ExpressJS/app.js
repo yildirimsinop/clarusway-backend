81c164be5dfd20c90d40d04ef41e30af5a51ece7
Umit Yildirim <umity@uia.no>
2023-09-25 20:18:14 +0200

    app

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -13,7 +13,13 @@ const app = express(); // run application on express.
 
 /* ENV */
 require("dotenv").config();
-const HOST = process.env.HOST || "http://localhost";
+const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 8000;
+/* ------------------------------------------------------- */
+/* HTTP_Methods & URLs */
 
-app.listen(PORT, () => console.log(`Running on ${HOST} : ${PORT}`));
+app.get("/", (request, response) => {});
+
+/* ------------------------------------------------------- */
+// app.listen(PORT, () => console.log(`Running on ${HOST}:${PORT}`))
+app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
