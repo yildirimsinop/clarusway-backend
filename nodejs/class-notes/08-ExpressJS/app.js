e62a39daf8d60c861db7cba1d8fafb8010478f81
Umit Yildirim <umity@uia.no>
2023-09-25 20:30:21 +0200

    response

--- nodejs/class-notes/08-ExpressJS/app.js
+++ nodejs/class-notes/08-ExpressJS/app.js
@@ -18,8 +18,11 @@ const PORT = process.env.PORT || 8000;
 /* ------------------------------------------------------- */
 /* HTTP_Methods & URLs */
 
-app.get("/", (request, response) => {});
+app.get("/", (request, response) => {
+  //? run response.send for print-out:
+  response.send("Welcome to Express");
+});
 
 /* ------------------------------------------------------- */
-// app.listen(PORT, () => console.log(`Running on ${HOST}:${PORT}`))
+// app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`))
 app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
