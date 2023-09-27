1973818d5c3941705224f95612d26e579603be14
Umit Yildirim <umity@uia.no>
2023-09-27 19:46:59 +0200

    callback

--- nodejs/class-notes/09-Middlewares/middelewares.js
+++ nodejs/class-notes/09-Middlewares/middelewares.js
@@ -10,8 +10,31 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
-//? Middleware functions has must be minimum three parameters.
-//? Last parameter for next().
+//? Middleware functions must be has three parameters.
+//? Last parameter is for next().
+
+/* next() for next Function */
+
+// Middleware:
+app.get("/", (req, res, next) => {
+  // Request/Response parametresi ile next() metoduna veri gönderebilirim.
+  req.customData = "Custom Data";
+  res.customDataInResponse = "Custom Data In Response";
+
+  next(); // Go to next Function.
+
+  // next() çalıştığı için çıktı vermeceyecek.
+  res.send({
+    message: "Middleware running",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    customData: [req.customData, res.customDataInResponse],
+    message: "Welcome",
+  });
+});
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
