957a42d5214430ca525395d5321a2009aaeb3b70
Umit Yildirim <umity@uia.no>
2023-09-27 20:52:25 +0200

    middle

--- nodejs/class-notes/09-Middlewares/middelewares.js
+++ nodejs/class-notes/09-Middlewares/middelewares.js
@@ -9,30 +9,126 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- */
-//? Middleware functions must be has three parameters.
+/* ------------------------------------------------------- *
+//? Middleware functions must be has three parameters. 
 //? Last parameter is for next().
 
-/* next() for next Function */
+//! next() for next Function:
 
 // Middleware:
-app.get("/", (req, res, next) => {
-  // Request/Response parametresi ile next() metoduna veri gönderebilirim.
-  req.customData = "Custom Data";
-  res.customDataInResponse = "Custom Data In Response";
+app.get('/', (req, res, next) => {
 
-  next(); // Go to next Function.
+    // Request/Response parametresi ile next() metoduna veri gönderebilirim.
+    req.customData = 'Custom Data With Request'
+    res.customDataWithResponse = 'Custom Data With Response'
+    
+    next() // Go to next Function. // Block Command
+    
+    // next() çalıştığı için çıktı vermeceyecek.
+    res.send({
+        message: 'Middleware running'
+    }) // Block Command
+})
 
-  // next() çalıştığı için çıktı vermeceyecek.
+app.get('/', (req, res) => {
+    res.send({
+        customData: [
+            req.customData,
+            res.customDataWithResponse
+        ],
+        message: 'Welcome to Home'
+    })
+})
+
+/* ------------------------------------------------------- *
+//! next() for next callBackFunction:
+
+const middleFunction1 = (req, res, next) => {
+
+    // console.log( req.query )
+    const skip = req.query.skip ?? false
+
+    req.customData = 'Custom Data With Request'
+    res.customDataWithResponse = 'Custom Data With Response'
+
+    if (skip) {
+        
+        // Bir sonraki bağımsız fonksiyona git:
+        next('route')
+
+    } else {
+        // Bir sonraki callback fonksiyona git:
+        next()
+
+    }
+}
+
+const middleFunction2 = (req, res, next) => {
+
+    // next()
+    
+    res.send({
+        customData: [
+            req.customData,
+            res.customDataWithResponse
+        ],
+        message: "Here is func2, first next() runned"
+    });
+
+}
+
+// //? add to function like callBack:
+// app.get('/', middleFunction1, middleFunction2, (req, res) => { ... }
+// //? It can be in array:
+app.get('/', [ middleFunction1, middleFunction2 ], (req, res) => {
+    res.send({
+        customData: [
+            req.customData,
+            res.customDataWithResponse
+        ],
+        message: 'Welcome to Home'
+    })
+})
+
+// next('route') ile çalıştı:
+app.get('/', (req, res) => {
+    res.send({
+        message: 'next route'
+    })
+})
+
+/* ------------------------------------------------------- */
+// Middlewares & USE:
+
+const middleFunction1 = (req, res, next) => {
+  // console.log( req.query )
+  const skip = req.query.skip ?? false;
+
+  req.customData = "Custom Data With Request";
+  res.customDataWithResponse = "Custom Data With Response";
+
+  if (skip) {
+    // Bir sonraki bağımsız fonksiyona git:
+    console.log("next-route çalıştı");
+    next("route");
+  } else {
+    // Bir sonraki callback fonksiyona git:
+    console.log("next çalıştı");
+    next();
+  }
+};
+
+app.use(middleFunction1);
+
+app.get("/", (req, res) => {
   res.send({
-    message: "Middleware running",
+    message: "first route",
   });
 });
 
 app.get("/", (req, res) => {
   res.send({
-    customData: [req.customData, res.customDataInResponse],
-    message: "Welcome",
+    message: "second route",
   });
 });
 
