4a096fe44d544c22804764ecca2814d904c60caa
Umit Yildirim <umity@uia.no>
2023-09-28 20:16:51 +0200

    up

--- nodejs/class-notes/10-Routers_Errors/index.js
+++ nodejs/class-notes/10-Routers_Errors/index.js
@@ -11,18 +11,28 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 //? "Router" is special app for URL control in ExpressJS.
+// app.get("/", (req, res) => {
+//   res.send({ mesasage: "Home Page" });
+// });
+// app.get("/", (req, res) => {
+//   res.send({ mesasage: "About Page" });
+// });
+// app.get("/", (req, res) => {
+//   res.send({ mesasage: "User Page" });
+// });
+
+const router = express.Router();
 
 app.get("/", (req, res) => {
   res.send({ mesasage: "Home Page" });
 });
-app.get("/", (req, res) => {
+app.get("/about", (req, res) => {
   res.send({ mesasage: "About Page" });
 });
-app.get("/", (req, res) => {
+app.get("/user/userId", (req, res) => {
   res.send({ mesasage: "User Page" });
 });
 
-const router = express.Router();
-
+app.use(router);
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
