b0259d39e63578325b9e96d263e5e96c12de2d7a
Umit Yildirim <umity@uia.no>
2023-09-30 11:24:44 +0200

    handler

--- nodejs/class-notes/11-Models-Sequeilze/server.js
+++ nodejs/class-notes/11-Models-Sequeilze/server.js
@@ -6,14 +6,29 @@
 const express = require("express");
 const app = express();
 
-app.all("/", (req, res) => {
-  res.send("WELCOME TO TODO API");
-});
-
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
+// Accept json data:
+app.use(express.json());
 
+app.all("/", (req, res) => {
+  res.send("WELCOME TO TODO API");
+});
+
+// continue from here...
+
+const errorHandler = (err, req, res, next) => {
+  const errorStatusCode = res.errorStatusCode ?? 500;
+  console.log("errorHandler runned.");
+  res.status(errorStatusCode).send({
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+  });
+};
+app.use(errorHandler);
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
