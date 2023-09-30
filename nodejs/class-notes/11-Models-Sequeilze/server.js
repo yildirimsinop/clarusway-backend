c4b2104189e4417f2ef56890f4224280f16bac40
Umit Yildirim <umity@uia.no>
2023-09-30 11:38:32 +0200

    npm i

--- nodejs/class-notes/11-Models-Sequeilze/server.js
+++ nodejs/class-notes/11-Models-Sequeilze/server.js
@@ -9,16 +9,19 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- */
 // Accept json data:
 app.use(express.json());
 
 app.all("/", (req, res) => {
   res.send("WELCOME TO TODO API");
 });
+/* ------------------------------------------------------- */
 
-// continue from here...
+//* SEQUELIZE
 
+const { Sequelize, DataTypes } = require("sequelize");
+
+/* ------------------------------------------------------- */
 const errorHandler = (err, req, res, next) => {
   const errorStatusCode = res.errorStatusCode ?? 500;
   console.log("errorHandler runned.");
