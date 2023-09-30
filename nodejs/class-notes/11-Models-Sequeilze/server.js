bf29abd56b59ba4fcd9beb5cf9cb19fefa303ca0
Umit Yildirim <umity@uia.no>
2023-09-30 13:26:07 +0200

    test

--- nodejs/class-notes/11-Models-Sequeilze/server.js
+++ nodejs/class-notes/11-Models-Sequeilze/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 // Accept json data:
-app.use(express.json())
+app.use(express.json());
 
 // app.all('/', (req, res) => {
 //     res.send('WELCOME TO TODO API')
@@ -20,21 +20,22 @@ app.use(express.json())
 /* ------------------------------------------------------- */
 //* TodoModel moved to todo.model.js
 
-app.use(require('./todo.router'))
+app.use(require("./todo.router"));
 
 /* ------------------------------------------------------- */
 
 const errorHandler = (err, req, res, next) => {
-    const errorStatusCode = res.errorStatusCode ?? 500
-    console.log('errorHandler runned.')
-    res.status(errorStatusCode).send({
-        error: true, // special data
-        message: err.message, // error string message
-        cause: err.cause, // error option cause
-        // stack: err.stack, // error details
-        body: req.body,
-    })
-}
-app.use(errorHandler)
+  const errorStatusCode = res.errorStatusCode ?? 500;
+  console.log("errorHandler runned.");
+  res.status(errorStatusCode).send({
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+    body: req.body,
+  });
+};
+
+app.use(errorHandler);
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
