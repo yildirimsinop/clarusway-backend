ac4cb0ca877a3763cd06fbd22b9eef7d62881508
Umit Yildirim <umity@uia.no>
2023-09-30 13:09:04 +0200

    up

--- nodejs/class-notes/11-Models-Sequeilze/todo.model.js
+++ nodejs/class-notes/11-Models-Sequeilze/todo.model.js
@@ -2,51 +2,57 @@
 /* -------------------------------------------------------
     EXPRESSJS - TODO Project with Sequelize
 ------------------------------------------------------- */
-
 //* SEQUELIZE
+//? npm i sequelize sqlite3
 
-const { Sequelize, DataTypes } = require("sequelize");
-// const sequelize = new Sequelize("sqlite:./db.sqlite3");
-const sequelize = new Sequelize(
-  "sqlite:" + (process.env.SQLITE || "./db.sqlite3")
-);
+// https://sequelize.org/docs/v6/getting-started/
+const { Sequelize, DataTypes } = require('sequelize')
+// Where is DB (DB Connection Details):
+// const sequelize = new Sequelize('sqlite:./db.sqlite3')
+const sequelize = new Sequelize('sqlite:' + (process.env.SQLITE || './db.sqlite3'))
 
-const Todo = sequelize.define("todo", {
-  // id: {
-  //   type: DataTypes.INTEGER,
-  //   allowNull: false, // default: true
-  //   unique: true,
-  //   field: "custom_column_name",
-  //   comment: "Description",
-  //   primaryKey: true,
-  //   autoIncrement: true,
-  // },
+// sequelize.define('tableName', { columns })
+const Todo = sequelize.define('todo', {
+    // https://sequelize.org/docs/v7/models/data-types/
+    // id: { //? Not need define ID field, it will create auto.
+    //     type: DataTypes.INTEGER,
+    //     unique: true,
+    //     allowNull: false, // default: true
+    //     field: 'custom_column_name', // Change column name
+    //     comment: 'Description',
+    //     primaryKey: true,
+    //     autoIncrement: true,
+    // },
+    title: {
+        type: DataTypes.STRING(64), // varchar(64)
+        allowNull: false,
+    },
+    description: DataTypes.TEXT, // ShortHand Using.
+    priority: { // 1: High, 0: Normal, -1: Low
+        type: DataTypes.TINYINT,
+        allowNull: false,
+        defaultValue: 0, // set default value.
+    },
+    isDone: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    //? Not need define "createdAt" & "updatedAt" fields.
+    // createdAt: false, // Unset
+    // updatedAt: false, // Unset
+})
 
-  title: {
-    type: DataTypes.STRING(64), // varchar(64)
-    allowNull: false,
-  },
+// Synchronization:
+//! SYNC MUST RUN ONCE!
+// sequelize.sync() // CREATE TABLE
+// sequelize.sync({ force: true }) // DROP & CREATE
+// sequelize.sync({ alter: true }) // TO BACKUP & DROP & CREATE & FROM BACKUP
 
-  description: DataTypes.TEXT, //ShortHand Using
-  priority: {
-    // 1: High, 0: Normal, -1: low
-    type: DataTypes.TINYINT,
-    allowNull: false,
-    defaultValue: 0,
-  },
+// Connect:
+sequelize.authenticate()
+    .then(() => console.log('* DB Connected *'))
+    .catch((err) => console.log('* DB Not Connected *', err))
 
-  isDone: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-});
 
-sequelize.sync({ alter: true });
-
-sequelize
-  .authenticate()
-  .then(() => console.log("*DB Connected *"))
-  .catch((err) => console.log("* DB Not Connected"));
-
-module.export = Todo;
+module.exports = Todo
--- nodejs/class-notes/11-Models-Sequeilze/todo.router.js
+++ nodejs/class-notes/11-Models-Sequeilze/todo.router.js
@@ -2,10 +2,31 @@
 /* -------------------------------------------------------
     EXPRESSJS - TODO Project with Sequelize
 ------------------------------------------------------- */
+const router = require('express').Router()
 
-const router = require("express").Router();
+const Todo = require('./todo.model')
 
-const Dodo = require(".");
-router.get("/", async (req, res) => {
-    const data 
-});
+// LIST:
+router.get('/', async (req, res) => {
+
+    // const data = await Todo.findAll()
+    const data = await Todo.findAndCountAll()
+    res.send({
+        error: false,
+        result: data
+    })
+})
+
+// CREATE:
+router.post('/', async (req, res) => {
+
+    const data = await Todo.create(req.body)
+    res.send({
+        error: false,
+        body: req.body, // Send Data
+        message: 'Created',
+        result: data // Receive Data
+    })
+})
+
+module.exports = router
