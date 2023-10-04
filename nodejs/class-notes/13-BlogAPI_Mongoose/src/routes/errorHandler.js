1039370c52635e209d81e83cc24274e7bbb4d627
Umit <umity@uia.no>
2023-10-04 19:46:49 +0200

    err

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/routes/errorHandler.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/routes/errorHandler.js
@@ -3,6 +3,12 @@
 // Catch Errors:
 
 module.exports = (err, req, res, next) => {
-    
+
     const errorStatusCode = res.errorStatusCode ?? 500
+
+    res.status(errorStatusCode).send({
+        error: true,
+        message: err.jessage,
+        cause: err.cause,
+    })
 }
