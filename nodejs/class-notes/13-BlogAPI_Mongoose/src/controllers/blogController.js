14311b55ad3b2738d036359999e5a48a54385da6
Umit <umity@uia.no>
2023-10-04 21:24:59 +0200

    list

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
@@ -11,5 +11,29 @@ const { BlogPost} = require('../models/blogModel')
 
 module.exports.BlogPost = {
 
+    list:  async (req, res) => {
+
+        const data = await BlogPost.find()
+
+        res.status(200).send({
+            error: false,
+            result: data
+        })
+
+    },
+    create: async (req, res) => {
+
+    },
+    read: async (req, res) => {
+
+    },
+    update: async (req, res) => {
+
+    },
+    delete: async (req, res) => {
+
+    },
+
+    
 
 }
