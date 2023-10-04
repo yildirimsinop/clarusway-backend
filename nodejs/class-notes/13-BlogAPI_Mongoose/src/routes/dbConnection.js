9744949e9bf6ae13e25e072fde86387e05fbe267
Umit <umity@uia.no>
2023-10-04 20:47:45 +0200

    db

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/routes/dbConnection.js
+++
@@ -1,7 +0,0 @@
-"user strict"
-
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017/')
-.then(() => console.log('* DB Connected'))
-.catch ((err)=> console.log(' * DB Not Connected * ', err))
