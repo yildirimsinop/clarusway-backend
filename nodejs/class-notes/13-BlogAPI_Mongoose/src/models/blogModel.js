2965a3d742cc74cf940880cc1cccde552579c8c8
Umit <umity@uia.no>
2023-10-04 20:37:47 +0200

    index

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
@@ -1,4 +1,12 @@
 "use strict"
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
-------------------------------------------------------- */
+// ------------------------------------------------------- */
+
+const mongoose = require('mongoose')
+
+const nameSchema = new mongoose.Schema({
+
+    fieldName: String
+})
+
