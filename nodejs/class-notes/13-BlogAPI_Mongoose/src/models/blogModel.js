3ec28a99e6adb1c49ba24df52d94943792055874
Umit <umity@uia.no>
2023-10-04 20:51:00 +0200

    model

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/models/blogModel.js
@@ -1,12 +1,34 @@
 "use strict"
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
-// ------------------------------------------------------- */
+------------------------------------------------------- */
 
 const mongoose = require('mongoose')
 
-const nameSchema = new mongoose.Schema({
+/* ------------------------------------------------------- */
 
-    fieldName: String
+const nameSchema = new mongoose.Schema({
+    
+    // _id: // AUTO CREATED
+
+    // fieldName: String // Shorthand Using
+    fieldName: {
+        type: String, // Veri tipi
+        defualt: null, // Default bir değer atayabiliriz.
+        trim: true, // Gelen veriyi trim'den geçir: trim(data)
+        select: true, // Show/UnShow
+        index: false, // Daha hızlı erişim olsun mu?
+        unique: false, // Benzersiz kayıt olsun mu?
+        required: [true, 'Error-Message'], // JSON data içinde gelmesi zorunlu mu?
+        enum: [[0, 1, 2, 3], 'Error-Message'], // Pattern/Constraint/Limit/Choices
+        validate: [function(data) { return true }, 'Error-Message'], // Veriyi filtreden geçiren fonksiyon.
+        get: function(data) { return true }, // Veri çağırırken çalıştırılacak fonksiyon
+        set: function(data) { return true }, // Veri kaydederken çalıştırılacak fonksiyon
+    }
+
+}, {
+    collection: 'collectionName', // Tablo ismi ne olsun?
+    timestamps: true, // Create and Manage 'createdAt' and 'updatedAt'
 })
 
+/* ------------------------------------------------------- */
