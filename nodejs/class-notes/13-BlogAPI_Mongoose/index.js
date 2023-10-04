bc9895da32a7ebaec8861ac6d9b50777e0308831
Umit <umity@uia.no>
2023-10-04 18:55:43 +0200

    BlogAPI

---
+++ nodejs/class-notes/13-BlogAPI_Mongoose/index.js
@@ -0,0 +1,29 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+/*
+ * npm init -y
+ * npm i express dotenv express-async-errors
+ * npm i mongoose
+*/
+
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+/* ------------------------------------------------------- */
+
+app.use(express.json())
+
+app.all('/', (req, res) => {
+    res.send('WELCOME TO BLOG API')
+})
+
+// continue from here...
+
+/* ------------------------------------------------------- */
+
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
---
+++ nodejs/class-notes/13-BlogAPI_Mongoose/readme.md
@@ -0,0 +1,33 @@
+# BLOG Project with Mongoose
+
+## MONGOOSE
+
+https://mongoosejs.com/
+
+### What? Why?
+
+![](./mongoose.png)
+
+## BLOG API
+
+### ERD:
+
+![ERD](./erdBlogAPI.png)
+
+### Folder/File Structure:
+
+```
+    .env
+    .gitignore
+    index.js
+    readme.md
+    src/
+        errorHandler.js
+        dbConnection.js
+        models/
+            blog.js
+        controllers/
+            blog.js
+        routes/
+            blog.js
+```
