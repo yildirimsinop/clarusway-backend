1fcc246aebb4396972fad3207fbebf266ec5f67a
Umit <umity@uia.no>
2023-10-04 21:49:38 +0200

    title

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/errorHandler.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/errorHandler.js
@@ -2,3 +2,16 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+// Catch Errors:
+
+module.exports = (err, req, res, next) => {
+
+    const errorStatusCode = res.errorStatusCode ?? 500
+
+    res.status(errorStatusCode).send({
+        error: true,
+        message: err.message,
+        cause: err.cause,
+        body: req.body // sending json data
+    })
+}
