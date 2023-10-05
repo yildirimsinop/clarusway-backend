0e2d630d8f3dbc8c1612780060f19adc7014b667
Umit <umity@uia.no>
2023-10-05 20:31:00 +0200

    user

--- nodejs/class-notes/14-BlogAPI_UserModel/src/routes/userRoute.js
+++ nodejs/class-notes/14-BlogAPI_UserModel/src/routes/userRoute.js
@@ -2,35 +2,19 @@
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
 const router = require('express').Router()
 
-// Call Controllers:
-const { BlogCategory, BlogPost } = require('../controllers/blogController')
+const { User } = require('../controllers/userController')
 
 // ------------------------------------------
-// BlogCategory
+// User
 // ------------------------------------------
-router.route('/category')
-    .get(BlogCategory.list)
-    .post(BlogCategory.create)
+router.route('/user')
+    .get(User.list)
+    .post(User.create)
 
-router.route('/category/:categoryId')
-    .get(BlogCategory.read)
-    .put(BlogCategory.update)
-    .delete(BlogCategory.delete)
-
-// ------------------------------------------
-// BlogPost
-// ------------------------------------------
-router.route('/post')
-    .get(BlogPost.list)
-    .post(BlogPost.create)
-
-router.route('/post/:postId')
-    .get(BlogPost.read)
-    .put(BlogPost.update)
-    .delete(BlogPost.delete)
-
-router.get('/category/:categoryId/posts', BlogPost.listCategoryPosts)
-
-module.exports = router
+router.route('/user/:userId')
+    .get(User.read)
+    .put(User.update)
+    .delete(User.delete)
