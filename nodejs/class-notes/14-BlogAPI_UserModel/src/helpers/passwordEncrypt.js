79e72e3abd54cce58d87a2334166777d1ed3db58
Umit <umity@uia.no>
2023-10-05 21:25:53 +0200

    password

--- nodejs/class-notes/14-BlogAPI_UserModel/src/helpers/passwordEncrypt.js
+++ nodejs/class-notes/14-BlogAPI_UserModel/src/helpers/passwordEncrypt.js
@@ -9,3 +9,8 @@ const loopCount = 10_000
 const charsCount = 32 // write 32 for 64
 
 const encType = 'sha512'
+
+module.exports = function (password) {
+    const encode = crypto.pbkdf2(password, keyCode, loopCount, charsCount, encType)
+    return encode.toString('hex')
+}
