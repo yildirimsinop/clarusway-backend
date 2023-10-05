de58c74ee47c8d1d24b6a74fa26c7e0c11ee7fbb
Umit <umity@uia.no>
2023-10-05 20:26:58 +0200

    usermodel

---
+++ nodejs/class-notes/14-BlogAPI_UserModel/src/models/userModel.js
@@ -0,0 +1,32 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: [true, 'Email field must be required.'],
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    firstName: String,
+
+    lastName: String,
+
+}, {
+    collection: 'users',
+    timestamps: true
+})
+
+module.exports = module
