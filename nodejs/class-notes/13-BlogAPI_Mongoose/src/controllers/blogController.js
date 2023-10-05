e1e65063ff66a2e56e09e84ca01b50d95430bb7e
Umit <umity@uia.no>
2023-10-05 14:09:21 +0200

    up

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/controllers/blogController.js
@@ -21,18 +21,19 @@ module.exports.BlogPost = {
 
         res.status(200).send({
             error: false,
+            count: data.length,
             result: data
         })
     },
 
     create: async (req, res) => {
         
-        // const data = await BlogPost.insertOne({
+        // const data = await BlogPost.create({
         //     fieldName: 'value',
         //     fieldName: 'value',
         //     fieldName: 'value',
         // })
-        const data = await BlogPost.insertOne(req.body)
+        const data = await BlogPost.create(req.body)
 
         res.status(201).send({
             error: false,
@@ -42,15 +43,38 @@ module.exports.BlogPost = {
     },
 
     read: async (req, res) => {
-        
+
+        // req.params.postId
+        // const data = await BlogPost.findById(req.params.postId)
+        const data = await BlogPost.findOne({ _id: req.params.postId })
+
+        res.status(200).send({
+            error: false,
+            result: data
+        })
+
     },
 
     update: async (req, res) => {
         
+        // const data = await BlogPost.findByIdAndUpdate(req.params.postId, req.body, { new: true }) // return new-data
+        const data = await BlogPost.updateOne({ _id: req.params.postId }, req.body)
+
+        res.status(202).send({
+            error: false,
+            body: req.body,
+            result: data, // update infos
+            newData: await BlogPost.findOne({ _id: req.params.postId })
+        })
+
     },
 
     delete: async (req, res) => {
         
+        const data = await BlogPost.deleteOne({ _id: req.params.postId })
+
+        res.sendStatus( (data.deletedCount >= 1) ? 204 : 404 )
+
     },
 
 }
