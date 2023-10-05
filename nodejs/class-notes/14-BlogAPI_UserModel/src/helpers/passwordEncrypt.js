b1ae3355415f67d01746964ea2e4950ca5f44bbe
Umit <umity@uia.no>
2023-10-05 21:22:35 +0200

    sha512

--- nodejs/class-notes/14-BlogAPI_UserModel/src/helpers/passwordEncrypt.js
+++ nodejs/class-notes/14-BlogAPI_UserModel/src/helpers/passwordEncrypt.js
@@ -3,3 +3,9 @@
 const crypto = require('node:crypto')
 
 const keyCode = process.env.SECRET_KEY || 'write_random_chars_to_here'
+
+const loopCount = 10_000
+
+const charsCount = 32 // write 32 for 64
+
+const encType = 'sha512'
