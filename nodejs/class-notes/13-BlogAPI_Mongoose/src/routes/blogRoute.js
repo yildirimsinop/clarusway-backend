d57072e330b54f42dac7ab8c1821a6c2f1c4433f
Umit <umity@uia.no>
2023-10-05 19:15:29 +0200

    router

--- nodejs/class-notes/13-BlogAPI_Mongoose/src/routes/blogRoute.js
+++ nodejs/class-notes/13-BlogAPI_Mongoose/src/routes/blogRoute.js
@@ -5,7 +5,20 @@
 const router = require('express').Router()
 
 // Call Controlers:
-const { BlogPost } = require('../controllers/blogController')
+const {BlogCategory, BlogPost } = require('../controllers/blogController')
+
+// ------------------------------------------
+// BlogCategory
+// ------------------------------------------
+router.route('/post')
+    .get(BlogCategory.list)
+    .post(BlogCategory.create)
+
+router.route('/post/:postId')
+    .get(BlogCategory.read)
+    .put(BlogCategory.update)
+    .delete(BlogCategory.delete)
+
 
 // ------------------------------------------
 // BlogPost
