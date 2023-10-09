f9d654ee02b2c07fc6da2817336a8ad6eb918222
Umit <umity@uia.no>
2023-10-09 19:54:37 +0200

    findOne

--- nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
+++ nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
@@ -20,15 +20,34 @@ module.exports = {
     },
     create: async (req, res) => {
 
+        const data = await Department.create(req.body)
+           res.status(201).send({
+            eroror: false,
+            data // data: data
+        })
+
     },
     read: async (req, res) => {
+
+        const data = await Department.findOne({_id: req.params.id})
+          res.status(200).send({
+            eroror: false,
+            data // data: data
+        })
 
     },
     update: async (req, res) => {
 
+        const data = await Department.updateOne ({_id: req.params.id}, req.body)
+         res.status(202).send({
+            eroror: false,
+            data,
+            new: await Department.findOne ({_id: req.params.id})
+         })
+
     },
     delete: async (req, res) => {
-
+ const data = await Department.deleteOne ({_id: req.params.id}, req.body)
     
     },
 }
