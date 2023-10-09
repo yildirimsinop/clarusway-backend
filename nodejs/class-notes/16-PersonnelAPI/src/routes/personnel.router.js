2e0094112b5227c90bb6b49657f5e4822973abaf
Umit <umity@uia.no>
2023-10-09 20:34:43 +0200

    route

--- nodejs/class-notes/16-PersonnelAPI/src/routes/personnel.router.js
+++ nodejs/class-notes/16-PersonnelAPI/src/routes/personnel.router.js
@@ -7,5 +7,20 @@ const router = require('express').Router()
 
 
 
+const department = require('../controllers/department.controller')
+
+// URL: /departments
+
+router.route('/')
+    .get(department.list)
+    .post(department.create)
+
+    router.route('/:id')
+    .get(department.read)
+    .put(department.update)
+    .patch(department.update)
+    .delete(department.delete)
+
+
 /* ------------------------------------------------------- */
 module.exports = router
