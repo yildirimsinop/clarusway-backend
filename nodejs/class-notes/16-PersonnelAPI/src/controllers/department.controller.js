1e4f33a56af7d110fce09c565198bd4f26423bd2
Umit <umity@uia.no>
2023-10-09 19:43:53 +0200

    controller

--- nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
+++ nodejs/class-notes/16-PersonnelAPI/src/controllers/department.controller.js
@@ -2,3 +2,27 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = require('../models/department.model')
+
+module.exports = {
+
+    list: async (req, res) => {
+
+        const data = await res.getModelList(Department)
+
+    },
+    create: async (req, res) => {
+
+    },
+    read: async (req, res) => {
+
+    },
+    update: async (req, res) => {
+
+    },
+    delete: async (req, res) => {
+
+    
+    },
+}
