10e930a9d7d64e2db53d44e231635f862b7c0f63
Umit <umity@uia.no>
2023-10-09 19:39:23 +0200

    model

--- nodejs/class-notes/16-PersonnelAPI/src/models/department.model.js
+++ nodejs/class-notes/16-PersonnelAPI/src/models/department.model.js
@@ -4,3 +4,15 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const DepartmentShcema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    }
+}, {
+    collation: "departments",
+    timestamps: true
+})
