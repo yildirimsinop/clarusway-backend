fc7b17994517bd7e692aad581787d8c064741bf9
Umit <umity@uia.no>
2023-10-11 21:16:22 +0200

    index

--- nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/index.js
+++ nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/index.js
@@ -7,25 +7,6 @@
     $ npm i cookie-session
     $ npm i jsonwebtoken
 */
-
-const express = require('express')
-const app = express()
-
-/* ------------------------------------------------------- */
-
-
-
-// continue from here...
-
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
-/*
-    $ npm i express dotenv mongoose express-async-errors
-    $ npm i cookie-session
-    $ npm i jsonwebtoken
-*/
 const express = require('express')
 const app = express()
 
@@ -58,25 +39,32 @@ app.use(require('cookie-session')({ secret: process.env.SECRET_KEY }))
 // res.getModelList():
 app.use(require('./src/middlewares/findSearchSortPage'))
 
-// Login/Logout Control Middleware
+// Cookie: Login/Logout Control Middleware
+// app.use(async (req, res, next) => {
+
+//     const Personnel = require('./src/models/personnel.model')
+
+//     req.isLogin = false
+
+//     if (req.session?.id) {
+
+//         const user = await Personnel.findOne({ _id: req.session.id })
+
+//         // if (user && user.password == req.session.password) {
+//         //     req.isLogin = true
+//         // }
+//         req.isLogin = user && user.password == req.session.password
+//     }
+//     console.log('isLogin: ', req.isLogin)
+
+//     next()
+// })
+
 app.use(async (req, res, next) => {
 
-    const Personnel = require('./src/models/personnel.model')
-
-    req.isLogin = false
-
-    if (req.session?.id) {
-
-        const user = await Personnel.findOne({ _id: req.session.id })
-
-        // if (user && user.password == req.session.password) {
-        //     req.isLogin = true
-        // }
-        req.isLogin = user && user.password == req.session.password
-    }
-    console.log('isLogin: ', req.isLogin)
-
     next()
+
+
 })
 
 /* ------------------------------------------------------- */
@@ -92,22 +80,12 @@ app.all('/', (req, res) => {
     })
 })
 
+// /auth
+app.use('/auth', require('./src/routes/auth.router'))
 // /departments
 app.use('/departments', require('./src/routes/department.router'))
 // /personnels
 app.use('/personnels', require('./src/routes/personnel.router'))
-
-/* ------------------------------------------------------- */
-
-// errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
-
-// RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
-
-/* ------------------------------------------------------- */
-// Syncronization (must be in commentLine):
-// require('./src/helpers/sync')()
 
 /* ------------------------------------------------------- */
 
