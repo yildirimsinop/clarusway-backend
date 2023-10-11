af902761085e46d5a76708c2a13fa238e69950ec
Umit <umity@uia.no>
2023-10-11 20:00:37 +0200

    access

--- nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/src/controllers/auth.controller.js
+++ nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/src/controllers/auth.controller.js
@@ -1,37 +1,67 @@
 "use strict"
-
-
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
 // JWT
 // npm i jsonwebtoken
 
-const Personnel = require ('../models/personnel.model')
+const Personnel = require('../models/personnel.model')
+
 module.exports = {
 
     login: async (req, res) => {
 
-        const {uername, password} = req.body
+        const { username, password } = req.body
 
-        if (username & password) {
+        if (username && password) {
 
-        const user = await Personnel.findOne({username, password})
+            const user = await Personnel.findOne({ username, password })
 
-        if (user) {
-            if (user.isActive) {
-                // login ok 
-            } else {res.errorStatusCode = 401
+            if (user) {
+
+                if (user.isActive) {
+                // Login OK
+
+                    const accessData = {
+                        _id: user._id,
+                        departmentId: user.departmentId,
+                        firstName: user.firstName,
+                        lastName: user.lastName,
+                        isActive: user.isActive,
+                        isAdmin: user.isAdmin,
+                        isLead: user.isLead,
+                    }
+
+                    const refreshData = {
+                        username: user.username,
+                        password: user.password
+                    }
+
+
+
+                } else {
+
+                    res.errorStatusCode = 401
+                    throw new Error('This account is not active.')
+                }
+            } else {
+
+                res.errorStatusCode = 401
                 throw new Error('Wrong username or password.')
-
             }
-        }else {
+        } else {
+
             res.errorStatusCode = 401
-                throw new Error('Please entry username and password.')
-        }
+            throw new Error('Please entry username and password.')
         }
     },
+
     refresh: async (req, res) => {
 
     },
+
     logout: async (req, res) => {
 
     },
+
 }
