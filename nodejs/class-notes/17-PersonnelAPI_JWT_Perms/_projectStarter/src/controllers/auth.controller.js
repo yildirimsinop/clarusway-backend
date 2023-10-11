d43bd643c3a624daa7f6e6b2cc950ea6b04b9975
Umit <umity@uia.no>
2023-10-11 19:49:58 +0200

    username

--- nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/src/controllers/auth.controller.js
+++ nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/src/controllers/auth.controller.js
@@ -1,9 +1,19 @@
 "use strict"
 
 
+// JWT
+// npm i jsonwebtoken
+
+const Personnel = require ('../models/personnel.model')
 module.exports = {
 
     login: async (req, res) => {
+
+        const {uername, password} = req.body
+
+        if (username & password)
+
+        const user = await Personnel,findOne({username, password})
 
     },
     refresh: async (req, res) => {
