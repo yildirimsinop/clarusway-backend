9c1a3a3372907c6516a1f80ed9618267ad9ae3d9
Umit <umity@uia.no>
2023-10-12 20:39:00 +0200

    render

--- nodejs/class-notes/18-PersonnelAPI_Log_Docs/src/swagger.js
+++ nodejs/class-notes/18-PersonnelAPI_Log_Docs/src/swagger.js
@@ -2,21 +2,32 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-// npm i swagger-autogen
-// https://swagger-autogen.github.io/docs/
-
 require('dotenv').config()
 const HOST = process.env?.HOST || '127.0.0.1'
 const PORT = process.env?.PORT || 8000
+/* ------------------------------------------------------- */
+// npm i swagger-autogen
+// https://swagger-autogen.github.io/docs/
+
+const swaggerAutogen = require('swagger-autogen')
+const packageJson = require('./package.json')
 
 const document = {
+	// info: {
+	// 	version: "1.0.0",
+	// 	title: "Personnel API",
+	// 	description: "Personnel Management API Service",
+	// 	termsOfService: "http://www.clarusway.com",
+	// 	contact: { name: "Clarusway", email: "qadir@clarusway.com" },
+	// 	license: { name: "BSD License", },
+	// },
 	info: {
-		version: "1.0.0",
-		title: "Personnel API",
-		description: "Personnel Management API Service",
+		version: packageJson.version,
+		title: packageJson.title,
+		description: packageJson.description,
 		termsOfService: "http://www.clarusway.com",
-		contact: { name: "Clarusway", email: "qadir@clarusway.com" },
-		license: { name: "BSD License", },
+		contact: { name: packageJson.author, email: "qadir@clarusway.com" },
+		license: { name: packageJson.license, },
 	},
 	host: `${HOST}:${PORT}`,
 	basePath: '/',
