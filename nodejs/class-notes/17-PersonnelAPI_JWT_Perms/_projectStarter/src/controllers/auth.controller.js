f3f82fc3cbbe177786a8b7a1def0add7bc75218c
Umit <umity@uia.no>
2023-10-12 19:44:56 +0200

    auth

--- nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/src/controllers/auth.controller.js
+++ nodejs/class-notes/17-PersonnelAPI_JWT_Perms/_projectStarter/src/controllers/auth.controller.js
@@ -7,11 +7,12 @@
 
 const jwt = require('jsonwebtoken')
 const Personnel = require('../models/personnel.model')
+const checkUserAndSetToken = require('../helpers/checkUserAndSetToken')
 
 module.exports = {
 
     login: async (req, res) => {
-
+        /*
         const { username, password } = req.body
 
         if (username && password) {
@@ -32,7 +33,7 @@ module.exports = {
                         isAdmin: user.isAdmin,
                         isLead: user.isLead,
                     }
-                    const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '30m' })
+                    const accessToken = jwt.sign(accessData, process.env.ACCESS_KEY, { expiresIn: '10m' })
 
                     const refreshData = {
                         username: user.username,
@@ -41,13 +42,11 @@ module.exports = {
                     const refreshToken = jwt.sign(refreshData, process.env.REFRESH_KEY, { expiresIn: '3d' })
 
                     res.send({
-                    error: false,
-                   
+                        error: false,
                         token: {
                             access: accessToken,
                             refresh: refreshToken
-                        
-                    }
+                        }
                     })
 
                 } else {
@@ -65,14 +64,48 @@ module.exports = {
             res.errorStatusCode = 401
             throw new Error('Please enter username and password.')
         }
+        */
+        const checkUser = await checkUserAndSetToken(req.body)
+        if (checkUser.error) {
+            res.errorStatusCode = 401
+            throw new Error(checkUser.message)
+        } else {
+            res.send(checkUser)
+        }
     },
 
     refresh: async (req, res) => {
 
+        const refreshToken = req.body?.token?.refresh || null
+
+        if (refreshToken) {
+
+            const jwtData = jwt.verify(refreshToken, process.env.REFRESH_KEY)
+
+            if (jwtData) {
+
+                const checkUser = await checkUserAndSetToken(jwtData, false)
+                if (checkUser.error) {
+                    res.errorStatusCode = 401
+                    throw new Error(checkUser.message)
+                } else {
+                    res.send(checkUser)
+                }
+
+            } else {
+                res.errorStatusCode = 401
+                throw new Error('Wroong JWT Token')
+            }
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Please entry token.refresh')
+        }
     },
 
     logout: async (req, res) => {
-
+        res.send({
+            error: false,
+            message: 'No need any doing for logout. You must deleted Bearer Token from your browser.'
+        })
     },
-
 }
