6edc6085594d65450fd1dfdb806687a93be68790
Umit <umity@uia.no>
2023-10-14 12:27:34 +0200

    user

--- nodejs/class-notes/19-PizzaAPI/src/models/user.js
+++ nodejs/class-notes/19-PizzaAPI/src/models/user.js
@@ -50,4 +50,5 @@ const UserSchema = new mongoose.Schema({
     timestamps: true
 })
 
+/* ------------------------------------------------------- */
 module.exports = mongoose.model('User', UserSchema)
