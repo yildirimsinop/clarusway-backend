c50d87ad6f749b5c4803633128f56cb66ee2fc1c
Umit <umity@uia.no>
2023-10-14 12:27:15 +0200

    toppi

--- nodejs/class-notes/19-PizzaAPI/src/models/pizza.js
+++ nodejs/class-notes/19-PizzaAPI/src/models/pizza.js
@@ -4,3 +4,38 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+// Pizza Model:
+
+const PizzaSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+
+    image: {
+        type: String,
+        trim: true,
+    },
+
+    price: {
+        type: Number,
+        required: true,
+    },
+
+    toppings: [ // push, pull
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Topping'
+        }
+    ]
+
+}, {
+    collection: 'pizzas',
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Pizza', PizzaSchema)
