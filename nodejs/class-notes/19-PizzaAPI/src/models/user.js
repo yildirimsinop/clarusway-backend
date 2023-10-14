2d68c573326de281d74cfe11f4c279cb0d6a6413
Umit <umity@uia.no>
2023-10-14 10:56:09 +0200

    user.js

--- nodejs/class-notes/19-PizzaAPI/src/models/user.js
+++ nodejs/class-notes/19-PizzaAPI/src/models/user.js
@@ -4,3 +4,36 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+const UserSchema = new mongoose.Schema ({
+
+    username : {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set: (passwrod) => passwordEncrypt(password)
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'Email field must be required'],
+        unique: [true, 'There is this email. Email field must be unique'],
+        validate: [
+            (email)=> email.includes('@') && email.includes('.'),
+            'Email type is not correct.'
+        ]
+    }
+
+
+
+}, {})
