a010cb767be0c7d73638fab44b70fbe3c1277fbb
Umit <umity@uia.no>
2023-10-14 10:03:42 +0200

    day19

---
+++ nodejs/class-notes/19-PizzaAPI/src/middlewares/authentication.js
@@ -0,0 +1,28 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+// $ npm i jsonwebtoken
+// app.use(authentication):
+
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+
+    const auth = req.headers?.authorization || null
+    const accessToken = auth ? auth.split(' ')[1] : null
+
+    req.isLogin = false
+    req.user = null
+
+    jwt.verify(accessToken, process.env.ACCESS_KEY, function (err, user) {
+        if (err) {
+            res.errorStatusCode = 401
+            return next(err)
+        } else {
+            req.isLogin = true
+            req.user = user
+        }
+    })
+    next()
+}
