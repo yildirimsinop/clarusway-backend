443d77f5e0f4552d8b2aff05042146ac64e74520
Umit <umity@uia.no>
2023-10-16 19:19:44 +0200

    API2

---
+++ nodejs/class-notes/20-PizzaAPI_2/index.js
@@ -0,0 +1,76 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+/*
+    $ mkdir logs
+    $ cp .env-sample .env
+    $ npm init -y
+    $ npm i express dotenv mongoose express-async-errors
+    $ npm i jsonwebtoken morgan
+*/
+const express = require('express')
+const app = express()
+
+/* ------------------------------------------------------- */
+// Required Modules:
+
+// envVariables to process.env:
+require('dotenv').config()
+const PORT = process.env?.PORT || 8000
+
+// asyncErrors to errorHandler:
+require('express-async-errors')
+
+/* ------------------------------------------------------- */
+// Configrations:
+
+// Connect to DB:
+const { dbConnection } = require('./src/configs/dbConnection')
+dbConnection()
+
+/* ------------------------------------------------------- */
+// Middlewares:
+
+// Accept JSON:
+app.use(express.json())
+
+// Run Logger:
+app.use(require('./src/middlewares/logger'))
+
+// res.getModelList():
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+/* ------------------------------------------------------- */
+// Routes:
+
+// HomePath:
+app.all('/', (req, res) => {
+    res.send({
+        error: false,
+        message: 'Welcome to PIZZA API',
+    })
+})
+
+// auth:
+app.use('/auth', require('./src/routes/auth'))
+// user:
+app.use('/users', require('./src/routes/user'))
+// order:
+app.use('/orders', require('./src/routes/order'))
+// pizza:
+app.use('/pizzas', require('./src/routes/pizza'))
+// topping:
+app.use('/toppings', require('./src/routes/topping'))
+
+/* ------------------------------------------------------- */
+
+// errorHandler:
+app.use(require('./src/middlewares/errorHandler'))
+
+// RUN SERVER:
+app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+
+/* ------------------------------------------------------- */
+// Syncronization (must be in commentLine):
+// require('./src/helpers/sync')()
---
+++ nodejs/class-notes/20-PizzaAPI_2/src/models/order.js
@@ -0,0 +1,49 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- */
+// Order Model:
+
+const OrderSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    pizzaId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pizza',
+        required: true,
+    },
+
+    size: {
+        type: String,
+        required: true,
+        enum: ['Small', 'Medium', 'Large', 'XLarge']
+    },
+
+    quantity: {
+        type: Number,
+        required: true,
+    },
+
+    price: {
+        type: Number,
+        required: true,
+    },
+
+    totalPrice: {
+        type: Number,
+    },
+
+}, {
+    collection: 'orders',
+    timestamps: true
+})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Order', OrderSchema)
