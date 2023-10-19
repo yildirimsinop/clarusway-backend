40ddc734061796fcf4de5342905096158bd38b84
Umit <umity@uia.no>
2023-10-19 21:31:24 +0200

    permissions

--- nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/middlewares/permissions.js
+++ nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/middlewares/permissions.js
@@ -7,7 +7,32 @@
 module.exports = {
 
     isLogin: (req, res, next) => {
-        
-    }
+
+        if (req.user) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login.')
+        }
+    },
+
+    isStaff: (req, res, next) => {
+         if (req.user && req.user.isStaff) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and to be Staff')
+        }
+
+    },
+    isAdmin: (req, res, next) => {
+        if (req.user && req.user.isAdmin) {
+            next()
+        } else {
+            res.errorStatusCode = 403
+            throw new Error('NoPermission: You must login and to be Admin')
+        }
+
+    },
 
 }
