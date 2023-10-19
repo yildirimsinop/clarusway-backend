f027f2015a847b4dd5ab686cea9c1c8349bec19a
Umit <umity@uia.no>
2023-10-19 19:54:36 +0200

    flight

--- nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/models/flight.js
+++ nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/models/flight.js
@@ -2,3 +2,24 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- *
+{
+    "flightNumber": "IS-AN-001",
+    "airline": "THY",
+    "departure": "ISTANBUL",
+    "departureDate": "2020-10-01 10:00:00",
+    "arrival": "ANKARA",
+    "arrivalDate": "2020-10-01 12:00:00",
+    "createdId": "652ceaa1bae9cde5e8a97522"
+  }
+/* ------------------------------------------------------- */
+// Flight Model:
+
+const FlightSchema = new mongoose.Schema({
+
+
+}, { collection: 'flights', timestamps: true })
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Flight', FlightSchema)
