a131ea516c7fdf615970d34a77a351ff9ced934c
Umit <umity@uia.no>
2023-10-19 19:59:57 +0200

    flight.js

--- nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/models/flight.js
+++ nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/models/flight.js
@@ -18,6 +18,47 @@ const { mongoose } = require('../configs/dbConnection')
 
 const FlightSchema = new mongoose.Schema({
 
+    flightNumber: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    airline: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    departure: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    departureDate: {
+        type: Date,
+        required: true
+    },
+
+    arrival: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    arrivalDate: {
+        type: Date,
+        required: true
+    },
+
+    createdId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
 
 }, { collection: 'flights', timestamps: true })
 
