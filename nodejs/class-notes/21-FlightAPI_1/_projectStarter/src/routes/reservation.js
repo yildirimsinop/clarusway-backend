fc7d0aa85cee9fcd71c4506361c9f89c120bb8d1
Umit <umity@uia.no>
2023-10-21 10:06:25 +0200

    reserv

--- nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/routes/reservation.js
+++ nodejs/class-notes/21-FlightAPI_1/_projectStarter/src/routes/reservation.js
@@ -6,9 +6,12 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/reservation:
 
+const permissions = require('../middlewares/permissions')
 const reservation = require('../controllers/reservation')
 
 // URL: /reservations
+
+router.use(permissions.isStaffOrAdmin)
 
 router.route('/')
     .get(reservation.list)
@@ -18,7 +21,7 @@ router.route('/:id')
     .get(reservation.read)
     .put(reservation.update)
     .patch(reservation.update)
-    .delete(reservation.delete)
+    .delete(permissions.isAdmin, reservation.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
