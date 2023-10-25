69d03e35409d60b2d6802759dd152c3135f0813a
Umit <umity@uia.no>
2023-10-25 22:07:14 +0200

    index

--- nodejs/class-notes/23-RentACar_1/_projectStarter/src/routes/index.js
+++ nodejs/class-notes/23-RentACar_1/_projectStarter/src/routes/index.js
@@ -8,6 +8,16 @@ const router = require('express').Router()
 
 // URL: /
 
+// auth:
+router.use('/auth', require('./auth'))
+// user:
+router.use('/users', require('./user'))
+// token:
+router.use('/tokens', require('./token'))
+// car:
+router.use('/cars', require('./car'))
+// reservation:
+router.use('/reservations', require('./reservation'))
 // document:
 router.use('/documents', require('./document'))
 
