d694536dd23317e3fe029afdce94e5fd20eda632
Umit <umity@uia.no>
2023-10-25 19:22:21 +0200

    db

--- nodejs/class-notes/23-RentACar_1/_projectStarter/src/configs/dbConnection.js
+++ nodejs/class-notes/23-RentACar_1/_projectStarter/src/configs/dbConnection.js
@@ -6,9 +6,9 @@
 
 const mongoose = require('mongoose')
 
-const dbConnection = function() {
+const dbConnection = function () {
     // Connect:
-    mongoose.connect(process.env.MONGODB)
+    mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log('* DB Connected * '))
         .catch((err) => console.log('* DB Not Connected * ', err))
 }
