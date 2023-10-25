7f8caacd5eae7e0c78007b82e2fec48a80a46c2e
Umit <umity@uia.no>
2023-10-25 22:06:22 +0200

    auth

--- nodejs/class-notes/23-RentACar_1/_projectStarter/src/middlewares/authentication.js
+++ nodejs/class-notes/23-RentACar_1/_projectStarter/src/middlewares/authentication.js
@@ -9,13 +9,18 @@ const Token = require('../models/token')
 
 module.exports = async (req, res, next) => {
 
-    const auth = req.headers?.authorization || null
-    const tokenKey = auth ? auth.split(' ')[1] : null
+    // Authorization: Token ...
+    // Authorization: ApiKey ...
+    // Authorization: X-API-KEY ...
+    // Authorization: x-auth-token ...
+    // Authorization: Bearer ...
+    const auth = req.headers?.authorization || null // Token ...tokenKey...
+    const tokenKey = auth ? auth.split(' ') : null // ['Token', '...tokenKey...']
 
-    if (tokenKey) {
-        const tokenData = await Token.findOne({ token: tokenKey })
+    if (tokenKey && tokenKey[0] == 'Token') {
+        const tokenData = await Token.findOne({ token: tokenKey[1] })
         if (tokenData) req.user = await User.findOne({ _id: tokenData.userId })
-    }   
+    }
 
     next()
 }
