2a44a8a4daaa3b2e81946576f0b7008fa2e38d3c
Umit <umity@uia.no>
2023-10-25 22:06:54 +0200

    user

--- nodejs/class-notes/23-RentACar_1/_projectStarter/src/models/user.js
+++ nodejs/class-notes/23-RentACar_1/_projectStarter/src/models/user.js
@@ -6,7 +6,7 @@ const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- *
 {
     "username": "admin",
-    "password": "1234",
+    "password": "aA*123456",
     "email": "admin@site.com",
     "firstName": "admin",
     "lastName": "admin",
@@ -15,7 +15,7 @@ const { mongoose } = require('../configs/dbConnection')
 }
 {
     "username": "test",
-    "password": "1234",
+    "password": "aA*123456",
     "email": "test@site.com",
     "firstName": "test",
     "lastName": "test",
@@ -24,3 +24,84 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // User Model:
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    firstName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+
+}, { collection: 'users', timestamps: true })
+
+/* ------------------------------------------------------- */
+// Schema Configs:
+
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+// save: Only Create
+UserSchema.pre(['save', 'updateOne'], function(next) {
+
+    // const emailRegExp = new RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$")
+    // const isEmailValidated = emailRegExp.test(this.email)
+    // const isEmailValidated = RegExp("^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$").test(this.email)
+    const isEmailValidated = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email)
+
+    if (isEmailValidated) {
+
+        const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&].{8,}$/.test(this.password)
+    
+        if (isPasswordValidated) {
+            
+            this.password = passwordEncrypt(this.password)
+
+            next() // Allow to save.
+        } else {
+            
+            next( new Error('Password not validated.') )
+        }
+    } else {
+        
+        next( new Error('Email not validated.') )
+    }
+})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('User', UserSchema)
