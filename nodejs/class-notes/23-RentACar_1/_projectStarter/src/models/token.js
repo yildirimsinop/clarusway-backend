5cce0f88ccf07aa347bff414c988a58c876a9337
Umit <umity@uia.no>
2023-10-25 22:06:46 +0200

    token

--- nodejs/class-notes/23-RentACar_1/_projectStarter/src/models/token.js
+++ nodejs/class-notes/23-RentACar_1/_projectStarter/src/models/token.js
@@ -10,3 +10,24 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // Token Model:
+
+const TokenSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true,
+    }, 
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        index: true,
+    }, 
+
+}, { collection: 'tokens', timestamps: true })
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Token', TokenSchema)
