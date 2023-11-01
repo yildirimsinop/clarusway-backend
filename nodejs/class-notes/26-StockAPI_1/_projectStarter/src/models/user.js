44c86c664f908c30933720557abcceeba8b4de48
Umit <umity@uia.no>
2023-11-01 18:18:10 +0100

    boolean

--- nodejs/class-notes/26-StockAPI_1/_projectStarter/src/models/user.js
+++ nodejs/class-notes/26-StockAPI_1/_projectStarter/src/models/user.js
@@ -4,3 +4,29 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({
+
+
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    
+}, 
+ password: {
+        type: String,
+        trim: true,
+        required: true
+       
+}, 
+ email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+       
+}, 
+
+{ collection: 'users', timestamps: true})
