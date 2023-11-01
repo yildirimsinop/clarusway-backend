712e94ceb61dacbfebe659d169bd6d87328b95c9
Umit <umity@uia.no>
2023-11-01 18:43:38 +0100

    data

--- nodejs/class-notes/26-StockAPI_1/_projectStarter/src/controllers/user.js
+++ nodejs/class-notes/26-StockAPI_1/_projectStarter/src/controllers/user.js
@@ -2,3 +2,27 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+
+// User Controller:
+
+const User = require ('../models/user')
+
+module.exports = {
+    list: async (req, res) => {
+
+        const data = await res.getModelList(User)
+        res.status(200).send(data)
+    },
+     create: async (req, res) => {
+
+    },
+     read: async (req, res) => {
+
+    },
+     update: async (req, res) => {
+
+    },
+     delete: async (req, res) => {
+
+    },
+}
