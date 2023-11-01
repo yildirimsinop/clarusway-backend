732acd40f73b45ea49c802a155d7c4db9976846d
Umit <umity@uia.no>
2023-11-01 19:17:41 +0100

    token

--- nodejs/class-notes/26-StockAPI_1/_projectStarter/src/models/user.js
+++ nodejs/class-notes/26-StockAPI_1/_projectStarter/src/models/user.js
@@ -39,6 +39,11 @@ const { mongoose } = require('../configs/dbConnection')
 
 const UserSchema = new mongoose.Schema({
 
+    // id: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     default: function() { return this._id }
+    // },
+
     username: {
         type: String,
         trim: true,
@@ -93,12 +98,13 @@ const UserSchema = new mongoose.Schema({
 
 const passwordEncrypt = require('../helpers/passwordEncrypt')
 
-UserSchema.pre(['save', 'updateOne'], function(next) {
+UserSchema.pre(['save', 'updateOne'], function (next) {
 
     // get data from "this" when create;
     // if process is updateOne, data will receive in "this._update"
     const data = this?._update || this
 
+    // email@domain.com
     const isEmailValidated = data.email
         ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) // test from "data".
         : true
@@ -107,25 +113,33 @@ UserSchema.pre(['save', 'updateOne'], function(next) {
 
         if (data?.password) {
 
+            // pass == (min 1: lowerCase, upperCase, Numeric, @$!%*?& + min 8 chars)
             const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(data.password)
-        
+
             if (isPasswordValidated) {
-                
+
                 this.password = data.password = passwordEncrypt(data.password)
                 this._update = data // updateOne will wait data from "this._update".
-                
+
             } else {
-                
-                next( new Error('Password not validated.') )
+
+                next(new Error('Password not validated.'))
             }
         }
 
         next() // Allow to save.
 
     } else {
-        
-        next( new Error('Email not validated.') )
+
+        next(new Error('Email not validated.'))
     }
+})
+/* ------------------------------------------------------- */
+// FOR REACT PROJECT:
+UserSchema.pre('init', function (data) {
+
+    data.id = data._id
+    data.createds = data.createdAt.toLocaleDateString('tr-tr')
 })
 /* ------------------------------------------------------- */
 module.exports = mongoose.model('User', UserSchema)
--- nodejs/class-notes/26-StockAPI_1/_projectStarter/src/routes/index.js
+++ nodejs/class-notes/26-StockAPI_1/_projectStarter/src/routes/index.js
@@ -10,10 +10,10 @@ const router = require('express').Router()
 
 // // auth:
 // router.use('/auth', require('./auth'))
-// // user:
-// router.use('/users', require('./user'))
-// // token:
-// router.use('/tokens', require('./token'))
+// user:
+router.use('/users', require('./user'))
+// token:
+router.use('/tokens', require('./token'))
 
 
 // // brand:
