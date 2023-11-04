5fec397f91a8f7590895795cb59ce8c4d7f4bfb6
Umit <umity@uia.no>
2023-11-04 12:33:49 +0100

    8010

--- nodejs/class-notes/28-StockAPI_3/backend/index.js
+++ nodejs/class-notes/28-StockAPI_3/backend/index.js
@@ -11,7 +11,7 @@ const app = express()
 // envVariables to process.env:
 require('dotenv').config()
 const HOST = process.env?.HOST || '127.0.0.1'
-const PORT = process.env?.PORT || 8001
+const PORT = process.env?.PORT || 8010
 
 // asyncErrors to errorHandler:
 require('express-async-errors')
