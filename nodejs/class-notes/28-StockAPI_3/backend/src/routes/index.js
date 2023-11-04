0d155028920f3109672123623a0d4f397627c461
Umit <umity@uia.no>
2023-11-04 10:05:58 +0100

    backend

---
+++ nodejs/class-notes/28-StockAPI_3/backend/src/routes/index.js
@@ -0,0 +1,40 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/:
+
+// URL: /
+
+// auth:
+router.use('/account/auth', require('./auth'))
+// call user.create for /account/register:
+const { create: userCreate } = require('../controllers/user')
+router.use('/account/register', userCreate)
+
+// user:
+router.use('/users', require('./user'))
+// token:
+router.use('/tokens', require('./token'))
+
+
+// brand:
+router.use('/brands', require('./brand'))
+// category:
+router.use('/categories', require('./category'))
+// firm:
+router.use('/firms', require('./firm'))
+// product:
+router.use('/products', require('./product'))
+// purchase:
+router.use('/purchases', require('./purchase'))
+// sale:
+router.use('/sales', require('./sale'))
+
+// document:
+router.use('/documents', require('./document'))
+
+/* ------------------------------------------------------- */
+module.exports = router
